beea4bb6927ff5b1fd4869e24fd1cb0dad4bbbc9
Kelly Duran <kaduran16@gmail.com>
2022-09-10 13:13:13 -0400

    commit tree 82e90b560dcd3bd8ebefcfc81f82c6f0358ea661

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password 
+
+    # def password=(new_password)
+    #     salt = BCrypt::Engine::generate_salt
+    #     self.password_digest = BCrypt::Engine::hash_secret(new_password, salt)
+    # end
+
+    # def authenticate(password)
+    #     salt = password_digest[0..28]
+    #     return nil unless BCrypt::Engine::hash_secret(password, salt) == self.password_digest
+    #     self 
+    # end
+end
